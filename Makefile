e873ca8e1d5b0d0e38c6d4edb0cddfe60ce9755f
Fabien Marteau <fabien.marteau@armadeus.com>
2019-09-18 15:29:21 +0200

    adding sbt arguments

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
 SBT = sbt
+#BACKEND=--backend-name verilator
+VCD=--generate-vcd-output on
+
 
 hdl:
 	$(SBT) "runMain mdio.Mdio"
+
+test:
+	$(SBT) "test:runMain mdio.TestMdio $(BACKEND) $(VCD)"
+
+mrproper:
+	-rm *.anno.json
+	-rm *.fir
+	-rm *.v
+	-rm -rf target
+	-rm -rf test_run_dir
+	-rm -rf project
+
--- src/test/scala/testmdio.scala
+++ src/test/scala/testmdio.scala
@@ -1,1 +1,19 @@
-package miim
+package mdio 
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+
+class TestMdio (dut: Mdio) extends PeekPokeTester(dut) {
+  println("Begin of Mdio test")
+  for (i <- 1 to 10) {
+    step(1)
+    println("step " + i)
+  }
+  println("End of Mdio test")
+}
+
+object TestMdio extends App {
+  chisel3.iotesters.Driver.execute(args, () => new Mdio()){
+    c => new TestMdio(c)
+  }
+}
